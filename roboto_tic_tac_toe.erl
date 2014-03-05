94ae69a32f28e94727d2ad96cea9027324ac00aa
Igor Bonadio <igorbonadio@gmail.com>
2014-03-04 22:37:00 -0300

    Adds tic tac toe's board definition

---
+++ roboto_tic_tac_toe.erl
@@ -0,0 +1,21 @@
+-module(roboto_tic_tac_toe).
+-compile(export_all).
+
+create() ->
+  {tic_tac_toe_board, {tic_tac_toe_row, empty, empty, empty},
+                      {tic_tac_toe_row, empty, empty, empty},
+                      {tic_tac_toe_row, empty, empty, empty}}.
+
+set({tic_tac_toe_board, Row0, Row1, Row2}, 0, ColumnNumber, Piece) ->
+  {tic_tac_toe_board, set_row(Row0, ColumnNumber, Piece), Row1, Row2};
+set({tic_tac_toe_board, Row0, Row1, Row2}, 1, ColumnNumber, Piece) ->
+  {tic_tac_toe_board, Row0, set_row(Row1, ColumnNumber, Piece), Row2};
+set({tic_tac_toe_board, Row0, Row1, Row2}, 2, ColumnNumber, Piece) ->
+  {tic_tac_toe_board, Row0, Row1, set_row(Row2, ColumnNumber, Piece)}.
+
+set_row({tic_tac_toe_row, _, Column1, Column2}, 0, Piece) ->
+  {tic_tac_toe_row, Piece, Column1, Column2};
+set_row({tic_tac_toe_row, Column0, _, Column2}, 1, Piece) ->
+  {tic_tac_toe_row, Column0, Piece, Column2};
+set_row({tic_tac_toe_row, Column0, Column1, _}, 2, Piece) ->
+  {tic_tac_toe_row, Column0, Column1, Piece}.
