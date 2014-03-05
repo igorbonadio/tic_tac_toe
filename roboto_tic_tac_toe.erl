1c05ea6922941737684bd92e253968b3e40a8ece
Igor Bonadio <igorbonadio@gmail.com>
2014-03-05 10:07:53 -0300

    Getting symbols of a tic tac toe game

--- roboto_tic_tac_toe.erl
+++ roboto_tic_tac_toe.erl
@@ -1,5 +1,5 @@
 -module(roboto_tic_tac_toe).
--export([create/0, set/4]).
+-export([create/0, set/4, get/3]).
 
 create() ->
   {tic_tac_toe_board, {tic_tac_toe_row, empty, empty, empty},
@@ -19,3 +19,17 @@ set_row({tic_tac_toe_row, Column0, _, Column2}, 1, Piece) ->
   {tic_tac_toe_row, Column0, Piece, Column2};
 set_row({tic_tac_toe_row, Column0, Column1, _}, 2, Piece) ->
   {tic_tac_toe_row, Column0, Column1, Piece}.
+
+get({tic_tac_toe_board, Row0, _, _}, 0, ColumnNumber) ->
+  get_row(Row0, ColumnNumber);
+get({tic_tac_toe_board, _, Row1, _}, 1, ColumnNumber) ->
+  get_row(Row1, ColumnNumber);
+get({tic_tac_toe_board, _, _, Row2}, 2, ColumnNumber) ->
+  get_row(Row2, ColumnNumber).
+
+get_row({tic_tac_toe_row, Column0, _, _}, 0) ->
+  Column0;
+get_row({tic_tac_toe_row, _, Column1, _}, 1) ->
+  Column1;
+get_row({tic_tac_toe_row, _, _, Column2}, 2) ->
+  Column2.
